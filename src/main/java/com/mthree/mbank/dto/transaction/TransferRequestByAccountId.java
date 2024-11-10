9b61a419ed0d01ee85993059e60d4a975575a493
antonbaider <email.b.anton@gmail.com>
2024-11-10 22:38:13 +0100

    v6/2

--- src/main/java/com/mthree/mbank/dto/transaction/TransferRequestByAccountId.java
+++ src/main/java/com/mthree/mbank/dto/transaction/TransferRequestByAccountId.java
@@ -4,22 +4,28 @@ import com.mthree.mbank.entity.User;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 
 @Setter
 @Getter
-public class TransferRequestByUserId {
+public class TransferRequestByAccountId {
     @NotNull(message = "Sender account ID is required")
+    @Schema(description = "ID of the sender's account", example = "1")
     private Long senderAccountId;
 
     @NotNull(message = "Receiver account ID is required")
+    @Schema(description = "ID of the receiver's account", example = "2")
     private Long receiverAccountId;
 
     @NotNull(message = "Amount is required")
+    @Schema(description = "Amount to be transferred", example = "100.00")
     private BigDecimal amount;
 
+    @Schema(description = "Sender user details", hidden = true)
     private User sender;
 
+    @Schema(description = "Receiver user details", hidden = true)
     private User receiver;
 }
