757ef460533b0ccb56283ba5bccdd967cf3aa7c6
antonbaider <email.b.anton@gmail.com>
2024-11-11 01:50:42 +0100

    v6/3

--- src/main/java/com/mthree/mbank/dto/user/UpdateUserRequest.java
+++ src/main/java/com/mthree/mbank/dto/user/UpdateUserRequest.java
@@ -1,24 +1,39 @@
 package com.mthree.mbank.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdateUserRequest {
 
-    @Size(max = 50, message = "First name cannot exceed 50 characters")
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[A-Za-z]+$", message = "First name must contain only letters")
+    @Schema(description = "First name of the user", example = "John")
     private String firstName;
 
-    @Size(max = 50, message = "Last name cannot exceed 50 characters")
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    @Pattern(regexp = "^[A-Za-z]+$", message = "Last name must contain only letters")
+    @Schema(description = "Last name of the user", example = "Doe")
     private String lastName;
 
     @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    @Schema(description = "Email address of the user", example = "john.doe@login.jsp.com")
     private String email;
 
-    @Size(min = 10, max = 15, message = "Phone number must be between 10 and 15 digits")
+    @Pattern(regexp = "^[0-9]{10,15}$", message = "Phone number must be between 10 and 15 digits")
+    @NotBlank(message = "Phone number is required")
+    @Schema(description = "Phone number of the user", example = "1234567890")
     private String phone;
 
-    @Size(min = 5, message = "Password must be at least 5 characters long")
+    @NotBlank(message = "Password is required")
+    @Size(min = 5, max = 50, message = "Password must be between 8 and 50 characters")
+    @Schema(description = "Password for login", example = "P@ssw0rd", accessMode = Schema.AccessMode.WRITE_ONLY)
     private String password;
 }
