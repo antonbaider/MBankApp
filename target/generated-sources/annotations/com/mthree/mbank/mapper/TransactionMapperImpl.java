5c1106c60696ee2b043a61970ede14981fc69fc1
antonbaider <email.b.anton@gmail.com>
2024-11-11 19:53:22 +0100

    v6/4

--- target/generated-sources/annotations/com/mthree/mbank/mapper/TransactionMapperImpl.java
+++ target/generated-sources/annotations/com/mthree/mbank/mapper/TransactionMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-11T01:50:05+0100",
+    date = "2024-11-11T19:52:18+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (OpenLogic)"
 )
 @Component
